90bc8c51ce3368c9f17cfa7866649c79f121e467
Ashutosh Varma <ashutoshvarma11@live.com>
2019-06-13 11:05:37 +0530

    clean code and added helper functions for classes

--- objects.py
+++ objects.py
@@ -1,16 +1,5 @@
 from enum import Enum
 
-class Grade(Enum):
-    A_Plus = 1
-    A = 2
-    B_Plus = 3
-    B = 4
-    C = 5
-    D = 6
-    E = 7
-    P = 8
-    F = 9
-
 
 class Subject:
     def __init__(self, paper_id, paper_code=None, name=None, credit=None, minor_max=None,
@@ -29,15 +18,19 @@ class Subject:
 
     @staticmethod
     def pass_marks():
-        #TODO: Temprariliy using hardcoded value, update it to scrap from pdf
+        # TODO: Temprariliy using hardcoded value, update it to scrap from pdf
         return 40
+
+    @staticmethod
+    def max_marks():
+        # TODO: Temprariliy using hardcoded value, update it to scrap from pdf
+        return 100
 
     def __str__(self):
         return "{self.paper_id}-{self.name}[{self.paper_code}]".format(self=self)
 
     def __eq__(self, other):
         return self.paper_id == other
-
 
 
 class Marks:
@@ -48,29 +41,77 @@ class Marks:
         self.total = total
         self.grade = grade
 
-    def get_subject(self):
-        pass
-
     def __str__(self):
         return "[{self.paper_id}] Minor-{self.minor}, Major-{self.major}, Total-{self.total}".format(self=self)
 
 
 class Result:
 
-    def __init__(self, semester, marks):
+    def __init__(self, semester, marks={}):
         self.semester = semester
         self.marks = marks
 
-    def get_num_drops(self):
-        pass
+    def get_mark_drops(self):
+        # for paper_id, mark in self.marks.items():
+        #     if mark.total < Subject.pass_marks():
+        #         yeild mark
+        return [item[1] for item in self.marks.items() if item[1].total < Subject.pass_marks()]
 
+    def get_num_drops(self):
+        return len(self.get_mark_drops())
+
+    def get_marks_by_paper(self, paper_id):
+        return self.marks[paper_id]
+
+    def get_marks(self, min_marks=0, max_marks=Subject.max_marks()):
+        """
+        Return the marks whose total is more than 'min_marks' and more than 'max_marks'
+        """
+        return [item[1] for item in self.marks.items() if min_marks <= item[1].total <= max_marks]
+
+    def add_mark(self, paper_id, mark):
+        if isinstance(paper_id, int):
+            self.marks[paper_id] = mark
+        else:
+            raise TypeError("paper_id should be of int type")
+
+    
 
 class Student:
     def __init__(self, roll_num, full_name=None, batch_year=None):
         self.name = full_name
         self.id = roll_num
         self.batch = batch_year
-        self.results = []
+        self.results = {}
+
+    def iter_results(self):
+        """
+        Return ilterable object of all results.
+        """
+        for sem in self.results:
+            yield self.results[sem]
+
+    def get_result_by_sem(self, sem):
+        return self.results[sem]
+
+    def add_result(self, res, semester):
+        """
+        Add the result to results dict.
+        If result for semester is already present then it ignores current.
+        """
+        if not semester in self.results:
+            self.update_result(res, semester)
+        # else:
+        #     raise Exception("Result for semester {} is already present.".format(semester))
+
+    def update_result(self, res, semester):
+        """
+        Update the result for given semester.
+        """
+        if isinstance(semester, int):
+            self.results[semester] = res
+        else:
+            raise TypeError("semester should be of int type")
 
     def __str__(self):
         return "{self.name} - {self.id} [{self.batch}]".format(self=self)
