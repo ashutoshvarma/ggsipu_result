3adba43c70d8aff4b71a5fd556611870ca1a2a29
Ashutosh Varma <ashutoshvarma11@live.com>
2019-06-27 22:06:10 +0530

    add Tests and Resources

---
+++ Tests/tests.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import unittest
+
+import ggsipu_result
+
+# Configure path environment
+TESTS_ROOT = os.path.abspath(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.dirname(TESTS_ROOT)
+RESOURCE_ROOT = os.path.join(PROJECT_ROOT, 'Resources')
+
+# Append project dir to PATH
+sys.path.append(PROJECT_ROOT)
+
+
+class data_process_TestCases(unittest.TestCase):
+
+    subject_data_file = os.path.join(RESOURCE_ROOT, 'CSE_Result', '1.txt')
+    result_data_file = os.path.join(RESOURCE_ROOT, 'CSE_Result', '58.txt')
+
+    subject_file = os.path.join(RESOURCE_ROOT, 'subjects.json')
+    result_file = os.path.join(RESOURCE_ROOT, 'results.json')
+
+
+    def test_has_page_subjects(self):
+        # Load data file
+        with open(self.subject_data_file, 'r') as inputfile:
+            self.assertTrue(ggsipu_result.has_page_subjects(inputfile.read()))
+
+    def test_has_page_results(self):
+        # Load data file
+        with open(self.result_data_file, 'r') as inputfile:
+            self.assertTrue(ggsipu_result.has_page_results(inputfile.read()))
+
+    def test_iter_subjects(self):
+        with open(self.subject_data_file, 'r') as inputfile, open(self.subject_file, 'r') as dumpf:
+            subjects = [ggsipu_result.iter_subjects(inputfile.read())]
+            json_dump = ggsipu_result.toJSON(subjects)
+            self.assertEqual(json_dump, dumpf.read())
+
+    def test_iter_results(self):
+        with open(self.result_data_file, 'r') as inputfile, open(self.result_file, 'r') as dumpf:
+            results = [ggsipu_result.iter_subjects(inputfile.read())]
+            json_dump = ggsipu_result.toJSON(results)
+            self.assertEqual(json_dump, dumpf.read())
