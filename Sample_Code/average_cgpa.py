2ff511ccb240d9982ca6583141e7c59d261c3dc2
Ashutosh Varma <ashutoshvarma11@live.com>
2019-06-29 19:13:25 +0530

    add new sample code

---
+++ Sample_Code/average_cgpa.py
@@ -0,0 +1,15 @@
+from ggsipu_result import iter_pages, iter_results, has_page_results
+
+FILE = "document1.pdf"
+
+cgpa = []
+
+for page in iter_pages(FILE):
+    if has_page_results(page):
+        for res in iter_results(page):
+            # Filtered for specific batch
+            if res.batch == 2018:
+                cgpa.append(res.cgpa)
+
+avg_cgpa = sum(i for i in cgpa)/len(cgpa)
+print(avg_cgpa)
